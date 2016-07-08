15052aab35982cfa41f4f4904d028293a4bea9d1
HorseGuy69 <zaitsev.anton@yahoo.com>
2016-07-09 00:39:49 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -3161,7 +3161,7 @@
                     if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                     else {
                         var from = chat.un;
-                        var msg = '[@' + from + '] ';
+                        var msg = '\/me [@' + from + '] ';
 
                         msg += basicBot.chat.afkremoval + ': ';
                         if (basicBot.settings.afkRemoval) msg += 'ON';
@@ -3254,7 +3254,6 @@
                                 func(i);
                             }
                         }
-                        
                         else {
                             return API.sendChat(msg);
                         }
