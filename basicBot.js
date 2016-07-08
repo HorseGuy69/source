19bc01f7979612b2a04a377f97cc813ee57a882e
NoisYmenT <noisyment@gmail.com>
2016-07-08 19:15:18 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1921,6 +1921,47 @@
                 }
             },
 
+////////////////////////////////////////////////
+            punchCommand: {
+                command: 'punch',
+                rank: 'user',
+                type: 'startsWith',
+                getPunch: function (chat) {
+                    var c = Math.floor(Math.random() * basicBot.chat.punchs.length);
+                    return basicBot.chat.punchs[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
+//////////////////////////////////////////////////
+
+
+
+
 /*
             cookieCommand: {
                 command: 'cookie',
